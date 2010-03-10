73af19356c267852baac62c876dfcad032016413
Soraya Sánchez <soraya.schz@gmail.com>
2010-03-10 21:34:44 +0000

    Minor changes
    git-svn-id: https://op4j.svn.sourceforge.net/svnroot/op4j/trunk/op4j-devutils@603 2d6b427a-3b9d-4b24-b9d1-46c41b8758ae

--- src/main/java/org/op4j/devutils/doc/FnsAptFilesGenerator.java
+++ src/main/java/org/op4j/devutils/doc/FnsAptFilesGenerator.java
@@ -3,6 +3,7 @@ package org.op4j.devutils.doc;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -29,11 +30,9 @@ public class FnsAptFilesGenerator {
 		try {
 			System.out.println("Working with file: " + file.getName());
 			
-			//TODO Can have \s between parameters..as FnNumber has
-			
 			Pattern pattern = Pattern.compile("(\\/\\*[\\w\\/\\*\\s\\{\\@\\}\\(\\)\\.]*\\*\\/)?" +
-					"\\s*public\\s+static\\s+(?:final)?(?:\\<[a-zA-Z\\,\\s]*\\>){0,1}\\s+((?:\\<[a-zA-Z\\,\\s]+\\>|[a-zA-Z])+)\\s+([a-zA-Z]+)\\s*" +
-					"\\(\\s*([a-zA-Z\\<\\>\\,\\s]*)\\)\\s*\\{");
+					"\\s*public\\s+static\\s+(?:final)?(?:\\<[a-zA-Z\\,\\s]*\\>){0,1}\\s+((?:\\<[a-zA-Z\\,\\s\\<\\>]+\\>|[a-zA-Z])+)\\s+([a-zA-Z]+)\\s*" +
+					"\\(\\s*([a-zA-Z\\?\\<\\>\\,\\s]*)\\)\\s*\\{");
 			
 			Matcher matcher = pattern.matcher(FileUtils
 					.readFileToString(file));
@@ -41,9 +40,6 @@ public class FnsAptFilesGenerator {
 			createTableHeader(outputFile);			
 			while (matcher.find()) {				
 				System.out.println("Match: " + matcher.group());
-				System.out.println("Method name: " + getFunctionName(matcher));
-				System.out.println("Return type: " + getReturnType(matcher));
-				System.out.println("Params: " + getParams(matcher));
 				
 				addRow(outputFile, matcher);				
 			}			
@@ -67,26 +63,61 @@ public class FnsAptFilesGenerator {
 			.split("\r\n"));
 	}
 	static String getReturnType(Matcher matcher) {
+		System.out.println("Return type: " + matcher.group(2));
 		return escape(matcher.group(2));
 	}
 	static String getFunctionName(Matcher matcher) {
+		System.out.println("Fn name: " + matcher.group(3));
 		return escape(matcher.group(3));
 	}
 	static List<String> getParams(Matcher matcher) {
-		//TODO Take into account Map<String, Object> map, boolean valid
-		// Trim + remove final
-		return Op.onArrayOf(Types.STRING, escape(matcher.group(4)).split(",")).forEach()
-			.exec(Ognl.asString("trim()")).exec(new IFunction<String, String>() {
-				public String execute(String object, ExecCtx ctx)
-						throws Exception {
-					if (StringUtils.startsWith(object, "final")) {
-						return StringUtils.substringAfter(object, "final");
-					} 
-					return object;
-				}				
-			}).endFor().toList().get();
-		
+		List<String> params = getParams(matcher.group(4));
+		System.out.println("Params: " + params);
+		return params;
 	}
+	
+	static List<String> getParams(String params) {
+		System.out.println("Extract params from: " + params);
+		
+		List<String> output = new ArrayList<String>();
+		
+		StringBuffer remaining = new StringBuffer(params);
+		if (remaining.indexOf(">") != -1) {
+			int nextParamStartIndex = 0;
+			int opened = 0;
+			for (int index = 0; index < remaining.length(); index++) {
+				if (remaining.charAt(index) == '<') {
+					opened++;
+					continue;
+				}
+				if (remaining.charAt(index) == '>') {
+					opened--;
+					continue;
+				}
+				if (remaining.charAt(index) == ',' && opened == 0) {
+					output.add(remaining.substring(nextParamStartIndex, index));
+					nextParamStartIndex = index + 1;
+				}
+			}	
+			if (opened == 0) {
+				output.add(remaining.substring(nextParamStartIndex));
+			}
+		} else {
+			output.addAll(Op.onArrayOf(Types.STRING, remaining.toString().split(",")).toList().get());
+		}
+		
+		return Op.onList(output).forEach().exec(Ognl.asString("trim()"))
+			.exec(new IFunction<String, String>() {
+				public String execute(String object, ExecCtx ctx)
+				throws Exception {
+					if (StringUtils.startsWith(object, "final")) {
+						return escape(StringUtils.substringAfter(object, "final").trim());
+					} 
+					return escape(object);
+				}
+			}).endFor().get();
+	}
+	
 	static String escape(String input) {
 		if (StringUtils.isNotEmpty(input)) {
 			return input.replaceAll("<", "\\\\<")
@@ -152,53 +183,59 @@ public class FnsAptFilesGenerator {
 	public static void main(String[] args) {
 		
 		// TODO Use input parameters instead
-		String inputFilePrefix = "C:\\Documents and Settings\\Daniel\\workspace\\op4j\\src\\main\\java\\org\\op4j\\functions\\";
-		String outputFilePrefix = "C:\\Documents and Settings\\Daniel\\workspace\\op4j\\src\\site\\apt\\";
+//		String inputFilePrefix = "C:\\Documents and Settings\\Daniel\\workspace\\op4j\\src\\main\\java\\org\\op4j\\functions\\";
+//		String outputFilePrefix = "C:\\Documents and Settings\\Daniel\\workspace\\op4j\\src\\site\\apt\\";
 		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnarray.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnArray.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnboolean.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnBoolean.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fncalendar.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnCalendar.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fndate.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnDate.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnlist.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnList.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnmap.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnMap.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnmath.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnMath.java"
-//		}));
-//		
+		String inputFilePrefix = "C:\\Development\\workspace-galileo\\op4j\\src\\main\\java\\org\\op4j\\functions\\";
+		String outputFilePrefix = "C:\\Development\\workspace-galileo\\op4j\\src\\site\\apt\\";
+				
+		generateAllFnsDoc(new File(outputFilePrefix, "fnarray.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnArray.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnboolean.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnBoolean.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fncalendar.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnCalendar.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fndate.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnDate.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnlist.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnList.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnmap.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnMap.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnmath.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnMath.java"
+		}));
+		
 		generateAllFnsDoc(new File(outputFilePrefix, "fnnumber.apt"), Arrays.asList(new String[] {
 				inputFilePrefix + "FnNumber.java"
 		}));
 		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnobject.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnObject.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnset.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnSet.java"
-//		}));
-//		
-//		generateAllFnsDoc(new File(outputFilePrefix, "fnstring.apt"), Arrays.asList(new String[] {
-//				inputFilePrefix + "FnString.java"
-//		}));
+		generateAllFnsDoc(new File(outputFilePrefix, "fnobject.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnObject.java"
+		}));
 		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnset.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnSet.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "fnstring.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnString.java"
+		}));
+		
+		generateAllFnsDoc(new File(outputFilePrefix, "team.apt"), Arrays.asList(new String[] {
+				inputFilePrefix + "FnMapOf.java"
+		}));
 		
 		System.out.println("All apt files have been generated");
 		
